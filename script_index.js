5fff095c9354ee4e521ea597e1addc4a92890277
TaniuOp <taniuop@gmail.com>
2021-10-12 10:39:08 +0200

    actualización con archivos locales

---
+++ script_index.js
@@ -0,0 +1,24 @@
+function sum(a, b){
+    return a + b;
+}
+console.log(sum(2,5));
+
+console.log('Hello World');
+
+let malename = 'Alex';
+console.log(malename);
+
+const interestRate = 3;
+interestRate = 7;
+console.log('interestRate');
+
+
+// una vez llega al undefined, deja de imprimir 
+let name;
+console.log(name);
+
+//por lo que no imprime esto: 
+function sum(c, d){
+    return c + d;
+}
+console.log(sum(4,5));
